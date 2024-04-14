6bdac1612469660b210a11404b0016436d71dca6
irkaneprobirka <liptonhwange@gmail.com>
2024-04-14 17:30:48 +0300

    new design on from-auth

--- views/site/login.php
+++ views/site/login.php
@@ -10,33 +10,44 @@ use yii\bootstrap5\Html;
 
 $this->title = 'Авторизация';
 ?>
+
 <div class="site-login">
 
+
+
     <div class="row">
-        <div class="col-lg-3">
-            <h1><?= Html::encode($this->title) ?></h1>
-            <?php $form = ActiveForm::begin([
-                'id' => 'login-form',
-                'fieldConfig' => [
-                    'template' => "{label}\n{input}\n{error}",
-                    'labelOptions' => ['class' => 'col-lg-1 col-form-label mr-lg-3'],
-                    'inputOptions' => ['class' => 'col-lg-3 form-control'],
-                    'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
-                ],
-            ]); ?>
+        <div class="col-md-8 col-lg-6 col-xxl-3 w-50 m-auto">
+            <div class="card mb-0">
+                <div class="card-body">
+                    <h1><?= Html::encode($this->title) ?></h1>
+                    <?php $form = ActiveForm::begin([
+                        'id' => 'login-form',
+                        'fieldConfig' => [
+                            'template' => "{label}\n{input}\n{error}",
+                            'labelOptions' => ['class' => 'form-label'],
+                            'inputOptions' => ['class' => 'form-control'],
+                            'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
+                        ],
+                    ]); ?>
 
-            <?= $form->field($model, 'login')->textInput(['autofocus' => true]) ?>
+                    <?= $form->field($model, 'login')->textInput(['autofocus' => true]) ?>
 
-            <?= $form->field($model, 'password')->passwordInput() ?>
+                    <?= $form->field($model, 'password')->passwordInput() ?>
 
-            <div class="form-group">
-                <div>
-                    <?= Html::submitButton('Войти', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    <div class="form-group">
+                        <div>
+                            <?= Html::submitButton('Войти', ['class' => 'btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2', 'name' => 'login-button']) ?>
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center">
+                        <p class="fs-4 mb-0 fw-bold">Еще нет аккаунта?</p>
+                        <?= Html::a('Создайте аккаунт', '/site/register', ['class' => 'text-primary fw-bold ms-2']) ?>
+                        <!-- <a class="text-primary fw-bold ms-2" href="./authentication-register.html">Создайте аккаунт</a> -->
+                    </div>
+
+                    <?php ActiveForm::end(); ?>
+
                 </div>
             </div>
-
-            <?php ActiveForm::end(); ?>
-
         </div>
     </div>
-</div>
